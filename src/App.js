a4f722c2379aed8f380905b4cce9f91698fff314
Julian Estefan <julian.y.estefan@gmail.com>
2018-12-31 14:51:15 -0300

    Added update checking to modal to improve performance. Moved Laout component to containers folder because of now it is a stateful component. Modified the routes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Layout from './components/Layput/Layout'
+import Layout from './containers/Layput/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './Modal.module.css'
 import Button from '../Button/Button'
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = (props) => (
+class Modal extends Component {
 
-    <>
-        <Backdrop show= {props.show} clicked = {props.closing} />
-        <div
-            className={styles.Modal}
-            style={{
-                opacity: props.show ? '1' : '0',
-                transform: props.show ? 'translateY(0)' : 'translateY(-450vh)'
-            }}>
-            {props.children}
-            <Button
-                clicked={props.closing}
-                type="Danger">
-                Cancel
-        </Button>
-            <Button
-                clicked={props.continuing}
-                type="Success">
-                Continue
-        </Button>
-        </div>
-    </>
-);
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show ;
+    }
 
-export default modal;
+    render() {
+        return (
+            <>
+                <Backdrop show={this.props.show} clicked={this.props.closing} />
+                <div
+                    className={styles.Modal}
+                    style={{
+                        opacity: this.props.show ? '1' : '0',
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-450vh)'
+                    }}>
+                    {this.props.children}
+                    <Button
+                        clicked={this.props.closing}
+                        type="Danger">
+                        Cancel
+                    </Button>
+                    <Button
+                        clicked={this.props.continuing}
+                        type="Success">
+                        Continue
+                    </Button>
+                </div>
+            </>
+        );
+    }
+}
+
+
+
+
+export default Modal;
