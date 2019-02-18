784851bfa4988f409452fd235dd7284f38e8e09f
Julian Estefan <julian.y.estefan@gmail.com>
2019-02-17 22:18:44 -0300

    Added lazy loading to Checkout component and orders.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import React, { Component, Suspense } from 'react';
+import { Route } from 'react-router-dom';
 
-import Layout from './containers/Layput/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-
+import Layout from './containers/Layput/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Spinner from './components/UI/Spinner/Spinner';
 import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
     return (
       <div>
-       <Layout>
-        <Route path = '/' exact component = {BurgerBuilder} />
-        <Route path='/checkout' component  = {Checkout} />
-        <Route path = '/orders' component = {Orders} />
-        <Auth />
-       </Layout>
+        <Layout>
+          <Route path='/' exact component={BurgerBuilder} />
+          <Suspense fallback={<Spinner />}>
+            <Route path='/checkout' component={React.lazy(() => import('./containers/Checkout/Checkout'))} />
+            <Route path='/orders' component={React.lazy(() => import('./containers/Orders/Orders'))} />
+          </Suspense>
+          <Auth />
+        </Layout>
       </div>
     );
   }
